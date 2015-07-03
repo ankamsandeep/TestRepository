bead9a2893996e8d67f759433343761abfbf2efa
Sandeep Ankam <sandeepankam@me.com>
2015-07-02 20:27:22 -0700

    Added Xcode project

---
+++ Test/Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Sandeep Ankam on 7/2/15.
+//  Copyright (c) 2015 Sandeep Ankam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
